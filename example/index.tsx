61bf44b38266efaa865f71c436cea0c6c0b0bd82
Gabriel Colque <gcolque@ext.sancorseguros.com>
2025-02-05 12:42:05 -0300

    Version 1.0.0-rc lista

--- example/index.tsx
+++ example/index.tsx
@@ -5,7 +5,7 @@ import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../.';
 
 const product = {
 	id: '1',
-	title: 'Coffee Mug - Card',
+	title: 'Coffee Mug - Card version 0.02',
 	// img: './coffee-mug.png',
 }
 
